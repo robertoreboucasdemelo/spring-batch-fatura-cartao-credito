3c6269976be12ee7238b108d31941faa09bb1593
Roberto Reboucas de Melo <roberto.reboucas@ibm.com>
2022-12-21 22:33:55 -0300

    Add Project

---
+++ src/main/java/com/moduretick/SpringFaturaCartaoCreditoJobApplication.java
@@ -0,0 +1,13 @@
+package com.moduretick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFaturaCartaoCreditoJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFaturaCartaoCreditoJobApplication.class, args);
+	}
+
+}
